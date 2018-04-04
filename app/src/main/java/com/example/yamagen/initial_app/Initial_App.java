00042cbcb0624f2792aa4618272330ced3b497bc
Yamagn <syankenpon@gmail.com>
2018-04-05 00:51:25 +0900

    ADD PROJECT

---
+++ app/src/main/java/com/example/yamagen/initial_app/Initial_App.java
@@ -0,0 +1,13 @@
+package com.example.yamagen.initial_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Initial_App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial__app);
+    }
+}
